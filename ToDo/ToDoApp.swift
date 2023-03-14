a3a11379f9cd44b6da3ea1713c47ba6bab15d4b7
idevF <124703341+idevF@users.noreply.github.com>
2023-03-14 15:59:46 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by idevF on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
